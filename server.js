d7578efbc6419e6812ee52df75ae8a9234f2601e
AntoineRai <antoine.raimand@gmail.com>
2024-05-30 14:57:44 +0200

    chore: Update server.js to handle socket room leaving

--- server.js
+++ server.js
@@ -6,17 +6,22 @@ const io = new Server(3002, {
     methods: ["GET", "POST"],
   },
 });
-  
+
 io.on("connection", (socket) => {
   console.log("Connexion établie avec le client " + socket.id);
 
   socket.on("join_room", (room_id) => {
     socket.join(room_id);
-    console.log("Le client " + socket.id + " a rejoint la room " + room_id);
+    console.log(`Client ${socket.id} a rejoint la room ${room_id}`);
 
     socket.on("send_message", ({ message }) => {
-      console.log("Le client " + socket.id + " a envoyé le message " + message + " dans la room " + room_id);s
       io.to(room_id).emit("message", message);
+    });
+
+    socket.on("leave_room", () => {
+      socket.leave(room_id);
+      console.log(`Client ${socket.id} a quitté la room ${room_id}`);
+      socket.emit("left_room", room_id);
     });
   });
 });
